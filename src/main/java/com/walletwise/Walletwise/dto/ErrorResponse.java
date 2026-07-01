501819f6d44a43e2b46858a587564dbf0e0eac13
Muhammad-Saud-13 <saudkhanlahore@gmail.com>
2026-07-01 18:50:00 +0500

    Fix error response construction

--- src/main/java/com/walletwise/Walletwise/dto/ErrorResponse.java
+++ src/main/java/com/walletwise/Walletwise/dto/ErrorResponse.java
@@ -2,10 +2,12 @@ package com.walletwise.Walletwise.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse {
     private LocalDateTime timestamp;
